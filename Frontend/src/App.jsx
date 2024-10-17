0150f200afb6f704c7ddcfb786baa21d55d55ada
ady09 <adeshgadage0908@gmail.com>
2024-10-18 02:46:59 +0530

    made changes in main

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,14 @@
-import {  Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-
-
-import { ProjectProvider } from './context/projectContext'
 import Layout from './components/Layout'
 import HomePage from './pages/Home'
 
-export default function App() {
+export default function App () {
   return (
-    <ProjectProvider>
-      
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </Layout>
-     
-    </ProjectProvider>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </Layout>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,18 +3,22 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ProjectProvider } from './context/projectContext.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <BrowserRouter>
-    <App />
-    <Toaster
-      position='top-center'
-      toastOptions={{
-        style: {
-          background: 'black', 
-          color: 'white',
-        },
-      }}
-    />
+    <ProjectProvider>
+      <App />
+      <Toaster
+        position='top-center'
+        toastOptions={{
+          style: {
+            background: 'black',
+            color: 'white',
+          },
+        }}
+      />
+    </ProjectProvider>
   </BrowserRouter>
 )
