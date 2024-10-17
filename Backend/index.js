fb5d465b534d034bb7db805c76f5d0d63cd976e9
ady09 <adeshgadage0908@gmail.com>
2024-10-18 03:34:52 +0530

    solved cors issue

--- Backend/index.js
+++ Backend/index.js
@@ -10,11 +10,22 @@ console.log('PORT:', process.env.PORT);
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Define the allowed origins
+const allowedOrigins = [
+    'https://te-assignment-2024-frontend.vercel.app',
+    'http://localhost:5173'
+];
 
+// Dynamic CORS options
 const corsOptions = {
-    origin: process.env.NODE_ENV === 'production' 
-        ? 'https://te-assignment-2024-frontend.vercel.app' 
-        : 'http://localhost:5173',
+    origin: (origin, callback) => {
+    
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true); 
+        } else {
+            callback(new Error('Not allowed by CORS')); 
+        }
+    },
     credentials: true,
 };
 
