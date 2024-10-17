dfb9800814a05ca6fa3cc4a4a41607406edae776
ady09 <adeshgadage0908@gmail.com>
2024-10-18 03:27:26 +0530

    added actual deployed url

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,34 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import projectRoutes from './routes/projectRoutes.js'
-import { connectToDB } from './config/db.js'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import projectRoutes from './routes/projectRoutes.js';
+import { connectToDB } from './config/db.js';
 
 console.log('MONGODB_URI:', process.env.MONGODB_URI);
 console.log('PORT:', process.env.PORT);
 
-const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
-
-connectToDB()
+const app = express();
+const port = process.env.PORT || 3000;
 
 
-app.use("/api/v1/projects", projectRoutes)
+const corsOptions = {
+    origin: process.env.NODE_ENV === 'production' 
+        ? 'https://te-assignment-2024-frontend.vercel.app' 
+        : 'http://localhost:5173',
+    credentials: true,
+};
 
+app.use(express.json());
+app.use(cors(corsOptions));
 
+connectToDB();
 
+app.use("/api/v1/projects", projectRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Te_Assignment_2024 Backend")
-})
+    res.send("Welcome to Te_Assignment_2024 Backend");
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
