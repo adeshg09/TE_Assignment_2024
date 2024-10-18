ea7e98e63dd1f137bf923fc0d8844fe01493cf28
Adesh Gadage <145595419+adeshg09@users.noreply.github.com>
2024-10-18 17:15:12 +0530

    Update apis.js

--- Frontend/src/services/apis.js
+++ Frontend/src/services/apis.js
@@ -1,5 +1,5 @@
 
-const Base_URL ="https://te-assignment-2024-backend-un5q.onrender.com/api/v1/projects/"
+const Base_URL ="https://te-assignment-2024backend.onrender.com/"
 
 
 export const projectEndpoints = {
